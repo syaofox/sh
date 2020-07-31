d0faec4d47430b3c2a5d606b6b00805ff033f6a7
syaofox <syaofox@gmail.com>
2020-07-31 20:05:09 +0800

    sync Fri Jul 31 20:05:09 CST 2020

--- xfce.sh
+++ xfce.sh
@@ -3,10 +3,10 @@
 set -e
 echo "Setting Mirrors"
 
-pacman -Syyy
+# pacman -Syyy
 pacman -S reflector
 reflector --verbose -c CN --sort rate  -a 6 -p https --save /etc/pacman.d/mirrorlist
-#Server = https://mirrors.bfsu.edu.cn/archlinux/$repo/os/$arch
+# Server = https://mirrors.bfsu.edu.cn/archlinux/$repo/os/$arch
 pacman -Syyy
 pacman -S --needed mtools dosfstools xdg-utils xdg-user-dirs reflector --noconfirm
 pacman -S --needed xorg xorg-xinit xorg-server --noconfirm
@@ -16,41 +16,44 @@ pacman -S --needed pulseaudio pulseaudio-alsa --noconfirm
 pacman -S --needed bluez bluez-utils --noconfirm
 pacman -S --needed mesa xf86-video-vmware haveged --noconfirm
 
-
+echo "enable Server"
 systemctl enable bluetooth
 systemctl start haveged
 systemctl enable fstrim.timer
 
+echo "Install Desktop"
 pacman -S --needed lightdm lightdm-webkit2-greeter xfce4 xfce4-goodies --noconfirm
 
 sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/g' /etc/lightdm/lightdm.conf
 systemctl enable lightdm
 
-
+echo "Install pkgs"
 pacman -S --needed xcape cifs-utils --noconfirm
 
+echo "Setting smb"
 mkdir -p /media/smb
 chown -R syaofox  /media/smb
-
 
 mkdir -p /media/smb/omvnas/me
 mkdir -p /media/smb/omvnas/kid
 mkdir -p /media/smb/openwrt/share
 
 echo '10.10.10.1	openwrt' >> /etc/hosts
-echo '10.10.10.3	openwrt' >> /etc/hosts
+echo '10.10.10.3	omvnas' >> /etc/hosts
 
 echo '//omvnas/share /media/smb/omvnas/share cifs  username=me,password=0928,vers=3.0,noauto,user 0 0' >> /etc/fstab
 echo '//omvnas/me /media/smb/omvnas/me cifs  username=me,password=0928,vers=3.0,noauto,user 0 0' >> /etc/fstab
 echo '//omvnas/kid /media/smb/omvnas/kid cifs  username=me,password=0928,vers=3.0,noauto,user 0 0' >> /etc/fstab
 echo '//openwrt/share /media/smb/openwrt/share cifs  username=me,password=0928,vers=2.0,noauto,user 0 0' >> /etc/fstab
 
+echo "Install pkgs"
 pacman --needed -S pavucontrol libcanberra libcanberra-pulse --noconfirm
 
 pacman --needed -S file-roller p7zip unrar unace lrzip squashfs-tools --noconfirm
 
 pacman --needed -S ffmpegthumbnailer ffmpegthumbs --noconfirm
 
+echo "Install Themes"
 pacman -S --needed arc-gtk-theme arc-icon-theme papirus-icon-theme --noconfirm
 
 
@@ -61,7 +64,7 @@ pacman -S --needed arc-gtk-theme arc-icon-theme papirus-icon-theme --noconfirm
 #su syaofox -c "makepkg -si PKGBUILD"
 
 
-
+echo "Configing Archlinuxcn"
 rm -rf /etc/pacman.d/gnupg
 pacman-key --init
 pacman-key --populate archlinux
@@ -71,14 +74,16 @@ echo "[archlinuxcn]" >> /etc/pacman.conf
 echo "Server = https://mirrors.bfsu.edu.cn/archlinuxcn/\$arch" >> /etc/pacman.conf
 
 pacman -Syy
-pacman -S  archlinuxcn-keyring
+pacman -S archlinuxcn-keyring --noconfirm
 pacman -S yay
 
-su syaofox -c "yay -S mint-themes mint-x-icons mint-y-icons"
-su syaofox -c "yay -S lightdm-webkit-theme-aether-git"
+echo "Install Themes"
+su syaofox -c "yay -S --needed mint-themes mint-x-icons mint-y-icons"
+su syaofox -c "yay -S --needed lightdm-webkit-theme-aether-git"
 
 # yay -S lightdm-webkit-theme-aether-git
 
+echo "Install lightdm-webkit Themes"
 git clone git@github.com:NoiSek/Aether.git /home/syaofox/.Aether
 cp --recursive /home/syaofox/.Aether /usr/share/lightdm-webkit/themes/Aether
 
