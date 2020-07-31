49fca0e8d5ea307ec38398cf6c5aa07fcf0bfd63
syaofox <syaofox@gmail.com>
2020-07-31 23:09:56 +0800

    sync 2020年 07月 31日 星期五 23:09:56 CST

--- xfce.sh
+++ xfce.sh
@@ -93,5 +93,5 @@ yay -S lightdm-webkit-theme-aether-git
 sudo cp -r /usr/share/lightdm-webkit/themes/lightdm-webkit-theme-aether /usr/share/lightdm-webkit/themes/Aether
 #git clone git@github.com:NoiSek/Aether.git ~/.Aether
 #sudo cp --recursive ~/.Aether /usr/share/lightdm-webkit/themes/Aether
-#sudo sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = lightdm-webkit-theme-aether #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
-#sudo sed -i 's/^\(#?greeter\)-session\s*=\s*\(.*\)/greeter-session = lightdm-webkit2-greeter #\1/ #\2g' /etc/lightdm/lightdm.conf
+sudo sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = lightdm-webkit-theme-aether #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
+sudo sed -i 's/^\(#?greeter\)-session\s*=\s*\(.*\)/greeter-session = lightdm-webkit2-greeter #\1/ #\2g' /etc/lightdm/lightdm.conf
