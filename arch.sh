a05b637939e464af7768344c5d7042567ac8ebf4
syaofox <syaofox@gmail.com>
2020-07-31 16:44:05 +0800

    sync Fri Jul 31 16:44:05 CST 2020

--- arch.sh
+++ arch.sh
@@ -13,6 +13,8 @@ function arch_chroot() {
     arch-chroot /mnt "/bin/bash" -c "${1}"
 }
 
+
+
 echo "时间同步"
 timedatectl set-ntp true
 
@@ -31,82 +33,80 @@ pacstrap -i /mnt base base-devel linux-lts linux-lts-headers linux-firmware inte
 echo "生成fstab"
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
-echo "切换系统"
-arch-chroot /mnt /bin/bash
+#echo "切换系统"
+#arch-chroot /mnt /bin/bash
 
 echo "设置交换文件"
 
-dd if=/dev/zero of=/swapfile bs=1M count=8192 status=progress #8G
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
+dd if=/dev/zero of=/mnt/swapfile bs=1M count=8192 status=progress #8G
+chmod 600 /mnt/swapfile
+mkswap /mnt/swapfile
+swapon /mnt/swapfile
 
-echo >> /etc/fstab
-echo "# 交换文件" >> /etc/fstab
-echo "/swapfile none swap defaults 0 0" >> /etc/fstab
+echo >> /mnt/etc/fstab
+echo "# 交换文件" >> /mnt/etc/fstab
+echo "/swapfile none swap defaults 0 0" >> /mnt/etc/fstab
 
 
 echo "设置语言"
 
-sed -i '/#en_US.UTF-8/s/^#//g' /etc/locale.gen
-sed -i '/#zh_CN.UTF-8/s/^#//g' /etc/locale.gen
-sed -i '/#zh_HK.UTF-8/s/^#//g' /etc/locale.gen
-sed -i '/#zh_TW.UTF-8/s/^#//g' /etc/locale.gen
-locale-gen
+sed -i '/#en_US.UTF-8/s/^#//g' /mnt/etc/locale.gen
+sed -i '/#zh_CN.UTF-8/s/^#//g' /mnt/etc/locale.gen
+sed -i '/#zh_HK.UTF-8/s/^#//g' /mnt/etc/locale.gen
+sed -i '/#zh_TW.UTF-8/s/^#//g' /mnt/etc/locale.gen
+arch_chroot "locale-gen"
 
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 #echo "LC_COLLATE=C" >> /etc/locale.conf
 
 echo "设置时区"
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-timedatectl set-ntp true
-hwclock --systohc --utc
+arch_chroot "echo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime"
+arch_chroot "timedatectl set-ntp true"
+arch_chroot "hwclock --systohc --utc"
 
 echo "设置hostname"
-echo "vm-arch" > /etc/hostname
+echo "vm-arch" > /mnt/etc/hostname
 vim /etc/hosts
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1 localhost" >> /etc/hosts
-echo "127.0.1.1 vm-arch.localdomain	vm-arch" >> /etc/hosts
+echo "127.0.0.1 localhost" >> /mnt/etc/hosts
+echo "::1 localhost" >> /mnt/etc/hosts
+echo "127.0.1.1 vm-arch.localdomain	vm-arch" >> /mnt/etc/hosts
 
 echo "设置网络"
-pacman -S networkmanager
-systemctl enable NetworkManager
+arch_chroot "pacman -S networkmanager"
+arch_chroot "systemctl enable NetworkManager"
 
 echo "设置网络"
-systemctl disable dhcpcd
+arch_chroot "systemctl disable dhcpcd"
 
 echo "root密码"
-passwd
+arch_chroot "passwd"
 
 echo "添加用户syaofox"
-useradd -m -g users -G wheel -s /bin/bash syaofox
-passwd syaofox
-echo 'syaofox ALL=(ALL:ALL) ALL' | EDITOR='tee -a' visudo
+arch_chroot "useradd -m -g users -G wheel -s /bin/bash syaofox"
+arch_chroot "passwd syaofox"
+arch_chroot "echo 'syaofox ALL=(ALL:ALL) ALL' | EDITOR='tee -a' visudo"
 
 
 echo "systemd-bootloader"
 
-pacman -S efibootmgr
+arch_chroot "pacman -S efibootmgr"
 
-bootctl install
+arch_chroot "bootctl install"
 
-sed -i '/#timeout 3/s/^#//g' /boot/loader/loader.conf
-
-
-
-echo "title Arch Linux" >> /boot/loader/entries/arch.conf
-echo "linux /vmlinuz-linux-lts" >> /boot/loader/entries/arch.conf
-echo "initrd /intel-ucode.img" >> /boot/loader/entries/arch.conf
-echo "initrd /initramfs-linux-lts.img" >> /boot/loader/entries/arch.conf
-echo "options root=\"PARTUUID=XXXX\" rw" >> /boot/loader/entries/arch.conf
-
-partuuid=$(blkid | grep archroot | sed -r "s/.*?PARTUUID=\"(.*?)\"/\1/g")
-sed -i "s/PARTUUID=XXXX/PARTUUID=${partuuid}/" /boot/loader/entries/arch.conf
+sed -i '/#timeout 3/s/^#//g' /mnt/boot/loader/loader.conf
 
 
 
+echo "title Arch Linux" >> /mnt/boot/loader/entries/arch.conf
+echo "linux /vmlinuz-linux-lts" >> /mnt/boot/loader/entries/arch.conf
+echo "initrd /intel-ucode.img" >> /mnt/boot/loader/entries/arch.conf
+echo "initrd /initramfs-linux-lts.img" >> /mnt/boot/loader/entries/arch.conf
+echo "options root=\"PARTUUID=XXXX\" rw" >> /mnt/boot/loader/entries/arch.conf
 
-exit
+arch_chroot "partuuid=$(blkid | grep archroot | sed -r "s/.*?PARTUUID=\"(.*?)\"/\1/g")"
+arch_chroot "sed -i "s/PARTUUID=XXXX/PARTUUID=${partuuid}/" /boot/loader/entries/arch.conf"
+
+
+
 
 mount -R /mnt
